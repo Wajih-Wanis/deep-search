3d32f05d6c6c21ab61a30588fcbc62cc475dc8c3
Wajih-Wanis <wajihwanis2000@gmail.com>
2025-03-05 23:43:12 +0100

    Added logs tp retriever

--- Utils/Retriever.py
+++ Utils/Retriever.py
@@ -21,6 +21,14 @@ current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 log_filename = os.path.join('logs', f"retriever_log_{current_time}.log")
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename=log_filename,
+    filemode='w',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 class Retriever(ABC):
   
     @abstractmethod
